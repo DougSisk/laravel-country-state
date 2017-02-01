8bd896a5875eb07ab8d68850ca67f5923a329fb8
Doug Sisk <doug@1scream.com>
2017-01-31 19:30:32 -0700

    New major version
    Replaces `phine/country` with `rinvex/country`. New package supports
    translated countries, so add in new config option. Remove controller &
    route to get states.

--- tests/CountryStateTest.php
+++ tests/CountryStateTest.php
@@ -4,29 +4,49 @@ use DougSisk\CountryState\CountryState;
 
 class CountryStateTest extends PHPUnit_Framework_TestCase
 {
+    private $countryState;
+
+    public function __construct()
+    {
+        parent::__construct();
+
+        $this->countryState = new CountryState;
+    }
+
     public function testGetCountries()
     {
-        $countryState = new CountryState;
-        $countries = $countryState->getCountries();
+        $countries = $this->countryState->getCountries();
 
         $this->assertRegExp("/([A-Z]{2})/", key($countries));
-        $this->assertEquals("United States", $countries['US']);
+        $this->assertEquals('United States', $countries['US']);
     }
 
     public function testGetCountryStates()
     {
-        $countryState = new CountryState;
-        $states = $countryState->getStates('US');
+        $states = $this->countryState->getStates('US');
 
         $this->assertRegExp("/([A-Z]{2})/", key($states));
-        $this->assertEquals("Hawaii", $states['HI']);
+        $this->assertEquals('Hawaii', $states['HI']);
+    }
+
+    public function testGetStateCode()
+    {
+        $stateCode = $this->countryState->getStateCode('Hawaii', 'US');
+
+        $this->assertEquals('HI', $stateCode);
     }
 
     public function testGetStateName()
     {
-        $countryState = new CountryState;
-        $stateName = $countryState->getStateName('HI', 'US');
+        $stateName = $this->countryState->getStateName('HI', 'US');
 
-        $this->assertEquals("Hawaii", $stateName);
+        $this->assertEquals('Hawaii', $stateName);
+    }
+
+    public function testGetTranslatedCountries()
+    {
+        $translatedCountries = $this->countryState->getCountries('spa');
+
+        $this->assertEquals('Estados Unidos', $translatedCountries['US']);
     }
 }
